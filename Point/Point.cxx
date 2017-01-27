b6b1868d54de35c940a0e9e0b8c99ea64d67015b
marmalmstudent <marmalm@student.chalmers.se>
2017-01-27 17:06:21 +0100

    managed to get the program working using standard library vectors

--- Point/Point.cxx
+++ Point/Point.cxx
@@ -26,12 +26,12 @@
 
 #define PI 3.141592653589793238463;
 
-void loadData(std::string filename, float* arr, int xSize, int ySize)
+void loadData(std::string filename,
+              std::vector<double> *arr)
 {
-  std::cout << &arr << std::endl;
-  for (int i = 0; i < xSize*ySize; i++)
+  for (std::vector<double>::iterator i = arr->begin(); i != arr->end(); i++)
     {
-      std::cout << arr[i] << std::endl;
+      std::cout << *i << std::endl;
     }
   std::string line;
   std::ifstream streamin(filename);
@@ -42,18 +42,9 @@ void loadData(std::string filename, float* arr, int xSize, int ySize)
           std::istringstream ss(line);
           std::istream_iterator<std::string> begin(ss), end;
           std::vector<std::string> arrayTokens(begin, end);
-          if (arrayTokens.size() == xSize*ySize)
+          for (int i = 0; i < arrayTokens.size(); i++)
             {
-              for (int i = 0; i < arrayTokens.size(); i++)
-                {
-                  arr[i] = std::stod(arrayTokens[i]);
-                }
-            }
-          else
-            {
-              std::cout << "Mismatch in size: file conatins " << arrayTokens.size()
-                        << " entries while supplied size is " << (xSize*ySize)
-                        << std::endl;
+              arr->push_back(std::stod(arrayTokens[i]));
             }
         }
       streamin.close();
@@ -65,42 +56,17 @@ void loadData(std::string filename, float* arr, int xSize, int ySize)
   return;
 }
 
-void createSurfaceArrayFromFile(float *x, float *y, float *z,
+void createSurfaceArrayFromFile(std::vector<double> *x,
+                                std::vector<double> *y,
+                                std::vector<double> *z,
                                 int xSize, int ySize,
                                 float xMin, float xMax,
                                 float yMin, float yMax,
                                 std::string filename)
 {
-  std::cout << &x << std::endl;
-  for (int i = 0; i < xSize*ySize; i++)
-    {
-      std::cout << x[i] << std::endl;
-    }
-  loadData("/home/marcus/git/vtkPlot/Point/X.txt", x, xSize, ySize);
-  std::cout << &y << std::endl;
-  for (int i = 0; i < xSize*ySize; i++)
-    {
-      std::cout << y[i] << std::endl;
-    }
-  loadData("/home/marcus/git/vtkPlot/Point/Y.txt", y, xSize, ySize);
-  std::cout << &z << std::endl;
-  for (int i = 0; i < xSize*ySize; i++)
-    {
-      std::cout << z[i] << std::endl;
-    }
-  loadData("/home/marcus/git/vtkPlot/Point/Z.txt", z, xSize, ySize);
-  /*
-  float xIncrement = (xMax-xMin)/(xSize-1);
-  float yIncrement = (yMax-yMin)/(ySize-1);
-  for (int i = 0; i < xSize; ++i)
-    {
-      for (int j = 0; j < ySize; ++j)
-        {
-          x[i] = xMin + i*xIncrement;
-          y[j] = yMin + j*yIncrement;
-        }
-    }
-  */
+  loadData("/home/marcus/git/vtkPlot/Point/X.txt", x);
+  loadData("/home/marcus/git/vtkPlot/Point/Y.txt", y);
+  loadData("/home/marcus/git/vtkPlot/Point/Z.txt", z);
 }
 
 void createSurfaceArray(float* x, float* y, float* z,
@@ -126,31 +92,30 @@ void createSurfaceArray(float* x, float* y, float* z,
     }
 }
 
-void calcZBounds(float* z, float* zBounds,
-		 int xSize, int ySize)
+void calcZBounds(std::vector<double> *arr, std::vector<double> *zBounds)
 {
   /*
     ----------------------------------------------------------------------------
     Find then maximum and minimum values of a given array to calibrate colormap.
     ----------------------------------------------------------------------------
    */
-  for (int i = 0; i < xSize; ++i)
+
+  for (std::vector<double>::iterator i = arr->begin(); i != arr->end(); i++)
     {
-      for (int j = 0; j < ySize; ++j)
-	{
-	  if (z[j*xSize +i] > zBounds[1])
-	    zBounds[1] = z[j*xSize +i];
-	  if (z[j*xSize +i] < zBounds[0])
-	    zBounds[0] = z[j*xSize +i];
-	}
+      if (*i > zBounds->at(1))
+        zBounds->at(1) = *i;
+      if (*i < zBounds->at(0))
+        zBounds->at(0) = *i;
     }
 }
 
 void createTopology(vtkSmartPointer<vtkPoints> points,
-		    vtkSmartPointer<vtkCellArray> vertices,
-		    vtkSmartPointer<vtkFloatArray> colormap,
-		    float* x, float* y, float* z,
-		    int xSize, int ySize)
+                    vtkSmartPointer<vtkCellArray> vertices,
+                    vtkSmartPointer<vtkFloatArray> colormap,
+                    std::vector<double> *x,
+                    std::vector<double> *y,
+                    std::vector<double> *z,
+                    int xSize, int ySize)
 {
   /*
     ----------------------------------------------------------------------------
@@ -175,25 +140,10 @@ void createTopology(vtkSmartPointer<vtkPoints> points,
       for (int j = 0; j < ySize-1; ++j)
 	{
 	  // The coordinates of the points in the cell
-	  p[0] = new float[3] {x[j*xSize+i], y[j*xSize+i], z[j*xSize+i]};
-	  p[1] = new float[3] {x[j*xSize+i+1], y[j*xSize+i+1], z[j*xSize+i+1]};
-	  p[2] = new float[3] {x[(j+1)*xSize+i], y[(j+1)*xSize+i], z[(j+1)*xSize+i]};
-	  p[3] = new float[3] {x[(j+1)*xSize+i+1], y[(j+1)*xSize+i+1], z[(j+1)*xSize+i+1]};
-	  /*
-	  // The coordinates of the points in the cell
-	  p[0][0] = x[i];
-	  p[0][1] = y[j];
-	  p[0][2] = z[j*xSize+i];
-	  p[1][0] = x[i+1];
-	  p[1][1] = y[j];
-	  p[1][2] = z[j*xSize+i+1];
-	  p[2][0] = x[i];
-	  p[2][1] = y[j+1];
-	  p[2][2] = z[(j+1)*xSize+i];
-	  p[3][0] = x[i+1];
-	  p[3][1] = y[j+1];
-	  p[3][2] = z[(j+1)*xSize+i+1];
-	  */
+	  p[0] = new float[3] {x->at(j*xSize+i), y->at(j*xSize+i), z->at(j*xSize+i)};
+	  p[1] = new float[3] {x->at(j*xSize+i+1), y->at(j*xSize+i+1), z->at(j*xSize+i+1)};
+	  p[2] = new float[3] {x->at((j+1)*xSize+i), y->at((j+1)*xSize+i), z->at((j+1)*xSize+i)};
+	  p[3] = new float[3] {x->at((j+1)*xSize+i+1), y->at((j+1)*xSize+i+1), z->at((j+1)*xSize+i+1)};
 	  // Insert the points
 	  pid[0] = points->InsertNextPoint(p[0]);
 	  pid[1] = points->InsertNextPoint(p[1]);
@@ -202,10 +152,10 @@ void createTopology(vtkSmartPointer<vtkPoints> points,
 	  // Insert the next cell
 	  vertices->InsertNextCell(4, pid);
 	  // Insert height to colormap
-	  colormap->InsertNextValue(z[j*xSize +i]);
-	  colormap->InsertNextValue(z[j*xSize +i+1]);
-	  colormap->InsertNextValue(z[(j+1)*xSize +i]);
-	  colormap->InsertNextValue(z[(j+1)*xSize +i+1]);
+	  colormap->InsertNextValue(z->at(j*xSize +i));
+	  colormap->InsertNextValue(z->at(j*xSize +i+1));
+	  colormap->InsertNextValue(z->at((j+1)*xSize +i));
+	  colormap->InsertNextValue(z->at((j+1)*xSize +i+1));
 	  delete[] p[0];
 	  p[0] = NULL;
 	  delete[] p[1];
@@ -242,9 +192,14 @@ void plotSurface(vtkRenderWindowInteractor *iren,
     ----------------------------------------------------------------------------
   */
   /* Create the surface */
+  /*
   float *x = new float[xSize];
   float *y = new float[ySize];
   float *z = new float[ySize*xSize];
+  */
+  std::vector<double> *x = new std::vector<double>();
+  std::vector<double> *y = new std::vector<double>();
+  std::vector<double> *z = new std::vector<double>();
   /*
   createSurfaceArray(x, y, z, xSize, ySize,
                      xMin, xMax, yMin, yMax);
@@ -253,12 +208,14 @@ void plotSurface(vtkRenderWindowInteractor *iren,
                              xMin, xMax, yMin, yMax,
                              filename);
   /* Find max and min for the colormap */
-  float* zBounds = new float[2] {z[0], z[0]};
-  calcZBounds(z, zBounds, xSize, ySize);
+  std::vector<double> *zBounds = new std::vector<double>();
+  zBounds->push_back(z->at(0));
+  zBounds->push_back(z->at(0));
+  calcZBounds(z, zBounds);
 
   /* Create the topology of the point (a vertex) */
   createTopology(points, vertices, colormap,
-                 x, y, z, xSize, ySize);
+                x, y, z, xSize, ySize);
   /* Clean up unused variables */
   /*
   delete[] x;
@@ -290,9 +247,9 @@ void plotSurface(vtkRenderWindowInteractor *iren,
   
   /* Visualize polygons as primitive grapnics */
   mapper->SetInputData(point);
-  mapper->SetScalarRange(zBounds[0], zBounds[1]); // colormap boundaries
-  delete[] zBounds;
-  zBounds = NULL;
+  mapper->SetScalarRange(zBounds->at(0), zBounds->at(1)); // colormap boundaries
+  //delete[] zBounds;
+  //zBounds = NULL;
   /* set the rendering scene */
   actor->SetMapper(mapper);
   actor->GetProperty()->SetOpacity(0.7);
@@ -316,7 +273,7 @@ void plotSurface(vtkRenderWindowInteractor *iren,
   renWin->AddRenderer(ren); // add renderer to window
   iren->SetRenderWindow(renWin); // set the window for the interactor
 }
-/*
+
 int main(int argc, char *argv[], char *envp[])
 {
   int nxpts, nypts;
@@ -344,7 +301,7 @@ int main(int argc, char *argv[], char *envp[])
   iren->Start();
   return EXIT_SUCCESS;
 }
-*/
+/*
 int functn(float *a, int val)
 {
   //std::cout << &a << std::endl;
@@ -386,3 +343,4 @@ int main(int argc, char *argv[], char *envp[])
     }
   std::cout << "" << std::endl;
 }
+*/
--- Point/Runprog.py
+++ Point/Runprog.py
@@ -2,11 +2,11 @@ import numpy as np
 import os
 
 
-xSize = 5
-ySize = 5
+xSize = 1001
+ySize = 1001
 filename = "/home/marcus/git/vtkPlot/Point/filename.txt"
-A, B = np.meshgrid(np.linspace(-np.pi, np.pi, xSize), np.linspace(-np.pi, np.pi, ySize))
-C = np.sin(A)*np.sin(B)
+A, B = np.meshgrid(np.linspace(-2*np.pi, 2*np.pi, xSize), np.linspace(-2*np.pi, 2*np.pi, ySize))
+C = np.sin(A**2)*np.sin(B**2)
 X = np.reshape(A, (-1, np.size(A)))
 Y = np.reshape(B, (-1, np.size(B)))
 Z = np.reshape(C, (-1, np.size(C)))
